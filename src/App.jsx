cfd143a9b08356f32d712ae5825afca846a15b22
Tadele Tadiwos <tadiwostadele389@gmail.com>
2026-05-02 10:51:24 -0900

    added MERN project

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
     {
   title: "Student Registration System",
   description: "A full-stack web application built with the MERN stack to manage student enrollments, profiles, and academic records efficiently.",
-  technologies: ["MongoDB", "Express", "React", "Node.js"],
+  technologies: "MongoDB, Express, React, Node.js"
 },
     {
       title: "Social Media Growth & Strategy",
